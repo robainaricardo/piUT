2817d4636629e9d92702d386cbd8f74f6e1d5257
Ricardo Robaina <ricardorobaina11@gmail.com>
2020-11-29 17:23:11 -0300

    versao 0.2

--- piUT.py
+++ piUT.py
@@ -9,19 +9,20 @@ DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 INTERVALO = 30
 ARQUIVO = "/var/log/temperatura"
-BD = "mongodb://192.168.122.1:27017/"
+BD_IP = "192.168.122.1"
+BD_PORTA = "27017"
 SENSOR = "bage-0001"
 
 # Main
 while True:
-    # Recebe a medição feita pelo sensor
+    # Recebe a medição feita pelo sensor e a data
     umidade, temperatura = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    data = datetime.datetime.now()
 
     # Verifica se a medição é válida
     if umidade is not None and temperatura is not None:
         # Imprime o valore recebido
-        data = datetime.datetime.now()
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(umidade, temperatura))
+        print(f"Sensor: {SENSOR}, Data: {data}, Umidade: {umidade}, Temperatura: {temperatura}\n")
 
         # Salva a medição em um Log
         salvarLog(umidade, temperatura, data)
@@ -37,13 +38,13 @@ while True:
 
 # Funções
 def salvarLog(umidade, temperatura, data):
-    with open(ARQUIVO), 'w') as log:
-        log.write('"Sensor" : {SENSOR}, "data": {data}, "umidade": {umidade}, "temperatura": {temperatura}\n')
+    with open(ARQUIVO, 'w') as log:
+        log.write(f"Sensor: {SENSOR}, Data: {data}, Umidade: {umidade}, Temperatura: {temperatura}\n")
 
 
 def salvarBD(umidade, temperatura, data):
-    client = MongoClient(BD)
+    client = MongoClient(BD_IP, BD_PORTA)
     db = client.temperatura
-    medicao = db.users.insert_one({"Sensor" : SENSOR, "data": data, "umidade": umidade, "temperatura": temperatura}).inserted_id()
+    medicao = db.insert_one({"Sensor" : SENSOR, "data": data, "umidade": umidade, "temperatura": temperatura}).inserted_id()
 
 
